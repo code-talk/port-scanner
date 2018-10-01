0d4b73a179af9bc4727880405b8ce5e1afbaeb7c
code-talk <43177677+code-talk@users.noreply.github.com>
2018-10-02 07:36:01 +0800

    Change some comment

--- scanner.py
+++ scanner.py
@@ -25,10 +25,7 @@ if __name__ == "__main__":
     # Check what time the scan started
     t1 = datetime.now()
 
-    # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-
     # We also put in some error handling for catching errors
-
     try:
         for port in range(options.min_port, options.max_port+1):  
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
