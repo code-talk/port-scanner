aa71816b57df7ffc8480f438fc6bbd46fa38cdf8
shuai.lou <shuai.lou@hulu.com>
2018-10-02 00:16:14 +0800

    refine code

--- scanner.py
+++ scanner.py
@@ -2,52 +2,58 @@
 import socket
 import subprocess
 import sys
+from optparse import OptionParser
 from datetime import datetime
 
-# Clear the screen
-subprocess.call('clear', shell=True)
+if __name__ == "__main__":
+    # Get options
+    parser = OptionParser()
+    parser.add_option("--host", help="host name", dest="host", type="string", default="127.0.0.1")
+    parser.add_option("--from", help="minimum port", dest="min_port", type="int", default=1)
+    parser.add_option("--to", help="maximum port", dest="max_port", type="int", default=1025)
 
-# Ask for input
-remoteServer    = raw_input("Enter a remote host to scan: ")
-remoteServerIP  = socket.gethostbyname(remoteServer)
+    (options, args) = parser.parse_args()
 
-# Print a nice banner with information on which host we are about to scan
-print "-" * 60
-print "Please wait, scanning remote host", remoteServerIP
-print "-" * 60
+    remoteServerIP  = socket.gethostbyname(options.host)
 
-# Check what time the scan started
-t1 = datetime.now()
+    # Print a nice banner with information on which host we are about to scan
+    print "-" * 60
+    print "Please wait, scanning remote host", remoteServerIP
+    print "from port {} to {}".format(options.min_port, options.max_port)
+    print "-" * 60
 
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+    # Check what time the scan started
+    t1 = datetime.now()
 
-# We also put in some error handling for catching errors
+    # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
 
-try:
-    for port in range(1,1025):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print "Port {}: 	 Open".format(port)
-        sock.close()
+    # We also put in some error handling for catching errors
 
-except KeyboardInterrupt:
-    print "You pressed Ctrl+C"
-    sys.exit()
+    try:
+        for port in range(options.min_port, options.max_port+1):  
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print "Port {}: 	 Open".format(port)
+            sock.close()
 
-except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
+    except KeyboardInterrupt:
+        print "You pressed Ctrl+C"
+        sys.exit()
 
-except socket.error:
-    print "Couldn't connect to server"
-    sys.exit()
+    except socket.gaierror:
+        print 'Hostname could not be resolved. Exiting'
+        sys.exit()
 
-# Checking the time again
-t2 = datetime.now()
+    except socket.error:
+        print "Couldn't connect to server"
+        sys.exit()
 
-# Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
+    # Checking the time again
+    t2 = datetime.now()
 
-# Printing the information to screen
-print 'Scanning Completed in: ', total
+    # Calculates the difference of time, to see how long it took to run the script
+    total =  t2 - t1
+
+    # Printing the information to screen
+    print 'Scanning Completed in: ', total
